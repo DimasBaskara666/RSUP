8f72355bdc6f7cfc595d0dd88a6d7e3d5d26d83c
DimasBaskara666 <dimas.bratakusuma@gmail.com>
2023-12-12 14:04:39 +0700

    First

---
+++ resources/views/mytext.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>mytext</title>
+</head>
+
+<body>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <td>No</td>
+                    <td>Nama</td>
+                    <td>Harga</td>
+                    <td>Stok</td>
+                    <td>Satu</td>
+                </tr>
+            </thead>
+            <tbody>
+                @forehjg.jgg
+            </tbody>
+        </table>khk
+    </div>
+</body>
+
+</html>B
